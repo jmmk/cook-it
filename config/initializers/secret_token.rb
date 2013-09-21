9ff0f0a601f9e4939d484bf0388a515a3885e6e5
Michael McLellan <jmikem825@gmail.com>
2013-09-21 16:34:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cookit::Application.config.secret_key_base = 'd030a2ec5fd77ac4d216382cc2a7e3ca29fbd303d937e6df964658ad2e2c063e119c337db77d3e924a5ac43b19fdb353ed278591869676a523b24a4f13184f7e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cookit::Application.config.session_store :cookie_store, key: '_cookit_session'
